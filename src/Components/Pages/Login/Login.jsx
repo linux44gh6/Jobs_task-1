87a609bf8434eafbeebee35f17480be621b8c99b
milon-hossain <abireshan32@gmial.com>
2024-08-16 00:15:29 +0600

    add login and register page

---
+++ src/Components/Pages/Login/Login.jsx
@@ -0,0 +1,37 @@
+
+const Login = () => {
+    return (
+        <div>
+            <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+      
+    </div>
+    <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
+      <form className="w-[400px] p-10">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered" required />
+          
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './Components/Pages/Home/Home';
+import Login from './Components/Pages/Login/Login';
+import Registry from './Components/Pages/Registration/Registry';
 
+// Define the router with routes
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path:'/register',
+        element:<Registry/>
+      }
+    ]
   },
 ]);
 
+// Render the application
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
