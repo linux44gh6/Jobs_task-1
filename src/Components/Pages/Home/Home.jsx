c0989cb2e9ff077ccce9b420a84f9927c50d1b91
milon-hossain <abireshan32@gmial.com>
2024-08-16 22:39:02 +0600

    add nav and footer

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import { Outlet,  } from "react-router-dom";
 import Product from "../Product/Product";
+import Nav from "../../../Shared/NavBar/Nav";
+import Footer from "../../../Shared/Footer/Footer";
 const Home = () => {
    
     return (
         <div>
            <Outlet></Outlet>
            <div>
+            <Nav></Nav>
             <Product></Product>
+            <Footer></Footer>
            </div>
         </div>
     );
